db405e20c20a798743d29c9782399a9a105423e8
Ayush ghosh <ghoshayush910@gmail.com>
2025-03-20 11:27:43 +0530

    roting is complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
-import React from 'react'
-import { BrowserRouter as Router, Route, BrowserRouter } from 'react-router-dom'
+// import React from 'react'
+import { BrowserRouter , Routes, Route } from 'react-router-dom'
 import { Box } from '@mui/material'
-
+import { Navbar, Feed, VideoDetail, ChannelDetail, SearchFeed } from './Component/Index'
 
 const App = () => {
   return (
     <BrowserRouter>
-      <Box sx={{ bgcolor: '#0000'}}>
-        Navbar
-        <Route path="/"  element={<Feed/>}/>
+      <Box sx={{backgroundColor: '#000', color: '#fff'}}>
+        <Navbar/>
+        <Routes>
+          <Route path="/"  element={<Feed/>} />
+          <Route path="/video/:id" element={<VideoDetail/>} />
+          <Route path="/Channel/:id" element={<ChannelDetail/>} />
+          <Route path="/Search/:SearchTearm" element={<SearchFeed/>} />
+        </Routes>
       </Box>
     </BrowserRouter>
   )
---
+++ src/Component/ChannelDetail.jsx
@@ -0,0 +1,11 @@
+// import React from 'react'
+
+function ChannelDetail() {
+  return (
+    <div>
+      ChannelDetai
+    </div>
+  )
+}
+
+export default ChannelDetail
