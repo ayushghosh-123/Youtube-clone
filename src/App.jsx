afe46023227373e223c47a26b0bdb8458c231aa3
Ayush ghosh <ghoshayush910@gmail.com>
2025-03-20 12:38:34 +0530

    searchbar work complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,21 @@
-// import React from 'react'
-import { BrowserRouter , Routes, Route } from 'react-router-dom'
-import { Box } from '@mui/material'
-import { Navbar, Feed, VideoDetail, ChannelDetail, SearchFeed } from './Component/Index'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Box } from '@mui/material';
+import { Navbar, Feed, VideoDetail, ChannelDetail, SearchFeed } from './Component/Index';
 
 const App = () => {
   return (
     <BrowserRouter>
-      <Box sx={{backgroundColor: '#000', color: '#fff'}}>
-        <Navbar/>
+      <Box sx={{ backgroundColor: '#000', color: '#fff' }}>
+        <Navbar />  
         <Routes>
-          <Route path="/"  element={<Feed/>} />
-          <Route path="/video/:id" element={<VideoDetail/>} />
-          <Route path="/Channel/:id" element={<ChannelDetail/>} />
-          <Route path="/Search/:SearchTearm" element={<SearchFeed/>} />
+          <Route path="/" element={<Feed />} />
+          <Route path="/video/:id" element={<VideoDetail />} />
+          <Route path="/channel/:id" element={<ChannelDetail />} />
+          <Route path="/search/:searchTerm" element={<SearchFeed />} />
         </Routes>
       </Box>
     </BrowserRouter>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -1,11 +1,30 @@
-// import React from 'react'
-import { Stack } from "@mui/material"
-import { Link } from "react-router-dom"
+import { Stack } from "@mui/material";
+import { Link } from "react-router-dom";
+
+import { logo } from "../Utils/Constant";
+import SearchBar from "./SearchBar";
 
 function Navbar() {
-  return (
-    <div>Navbar</div>
-  )
+  return (  
+    <Stack
+      direction="row"
+      alignItems="center"
+      p={2}
+      sx={{ 
+        backgroundColor: "#000",
+        color: "#fff",
+        position: "sticky",
+        top: 0,
+        justifyContent: "space-between"
+      }}
+    >
+      <Link to="/" style={{ display: "flex", alignItems: "center" }}>
+        <img src={logo} alt="logo" height={45} width={35}/>
+      </Link>
+      <SearchBar/>
+    </Stack>
+  );
 }
 
-export default Navbar
+export default Navbar;
+
